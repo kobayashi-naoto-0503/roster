17effb3f354b41248f247ae99ed489f01ca91d43
naoto <k.d.naoto.53@gmail.com>
2021-08-19 13:38:35 +0000

    テーブル作り途中

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -2,6 +2,9 @@ class MainsController < ApplicationController
   before_action :user_logged_in?
   
   def index
+    @now = Date.current
+    @day = @now.end_of_month.day.to_i
+    @last_day = Date.new(Time.now.year, Time.now.month, -1).day
     @nurses = Nurse.all
     @nurse = Nurse.find_by(id: params[:id])
   end
