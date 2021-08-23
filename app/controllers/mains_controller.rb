05846afbb23d39f640fccd0e85a611f3606689ab
naoto <k.d.naoto.53@gmail.com>
2021-08-23 02:14:35 +0000

    テーブルの日付完成

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -3,7 +3,8 @@ class MainsController < ApplicationController
   
   def index
     @now = Date.current
-    @day = @now.end_of_month.day.to_i
+    @next_month = @now.next_month
+    @day = @next_month.end_of_month.day.to_i
     @last_day = Date.new(Time.now.year, Time.now.month, -1).day
     @nurses = Nurse.all
     @nurse = Nurse.find_by(id: params[:id])
