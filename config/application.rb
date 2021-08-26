dc597c200474acd8cb2b9c066c40e3b1bd609be8
naoto <k.d.naoto.53@gmail.com>
2021-08-26 13:25:12 +0000

    テーブルのcss作成

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Roster
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     
+    config.paths.add 'lib', eager_load: true
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    
     config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
