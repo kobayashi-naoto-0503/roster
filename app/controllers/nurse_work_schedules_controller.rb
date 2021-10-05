b808a6dbe5da3041f91314ed10dbbbc482472abb
naoto <k.d.naoto.53@gmail.com>
2021-10-05 13:30:03 +0000

    work_scheduleのリファクタリング完了

--- app/controllers/nurse_work_schedules_controller.rb
+++ app/controllers/nurse_work_schedules_controller.rb
@@ -20,6 +20,7 @@ class NurseWorkSchedulesController < ApplicationController
     @day = Date.current.next_month.end_of_month.day.to_i #現在から見て1ヶ月後の日時の最終日を数値型で取得
     @nurses = Nurse.all
     @hope_holidays = HopeHoliday.all
+    @leaves = Leave.all
   end
   
   def edit
